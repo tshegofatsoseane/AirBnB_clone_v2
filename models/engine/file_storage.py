584bbc2899690084fffc87b2b4bb08179c33141a
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2024-01-01 18:20:03 -0800

    Update

---
+++ models/engine/file_storage.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""Define the FileStorage class."""
+import json
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
+class FileStorage:
+    """An abstracted storage engine.
+
+    Attributes:
+        __file_path (str): File to save objects.
+        __objects (dict): instantiated objects dictionary.
+    """
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self, cls=None):
+        """Dictionary of instantiated objects in __objects.
+
+        Return a dictionary of objects of that type, if a cls is specified.
+        else, return the __objects dictionary.
+        """
+        if cls is not None:
+            if type(cls) == str:
+                cls = eval(cls)
+            cls_dict = {}
+            for k, v in self.__objects.items():
+                if type(v) == cls:
+                    cls_dict[k] = v
+            return cls_dict
+        return self.__objects
+
+    def new(self, obj):
+        """ __objects obj with key <obj_class_name>.id."""
+        self.__objects["{}.{}".format(type(obj).__name__, obj.id)] = obj
+
+    def save(self):
+        """__objects to the JSON file __file_path."""
+        odict = {o: self.__objects[o].to_dict() for o in self.__objects.keys()}
+        with open(self.__file_path, "w", encoding="utf-8") as f:
+            json.dump(odict, f)
+
+    def reload(self):
+        """Deserialize the JSON file __file_path to __objects."""
+        try:
+            with open(self.__file_path, "r", encoding="utf-8") as f:
+                for o in json.load(f).values():
+                    name = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(name)(**o))
+        except FileNotFoundError:
+            pass
+
+    def delete(self, obj=None):
+        """Delete given object from __objects."""
+        try:
+            del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
+        except (AttributeError, KeyError):
+            pass
+
+    def close(self):
+        """Call the reload method."""
+        self.reload()
