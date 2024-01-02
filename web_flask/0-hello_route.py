70e34d06876f1ecf3687ca54f261b7950ad670a8
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2024-01-01 16:40:35 -0800

    Update

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,19 @@
-""" This app starts a Flask Web Application """
+#!/usr/bin/python3
+"""This app starts a Flask web application.
+Listens on 0.0.0.0, port 5000.
+Routes:
+    /: Print 'Hello HBNB!'
+"""
 from flask import Flask
+
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """ Print a Message """
-    return 'Hello HBNB!'
+    """Print 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     """ Main Function """
