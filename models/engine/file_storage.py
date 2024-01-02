ec95a7eeef04f01e8d9f7bd03345742036c7d41e
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2024-01-01 18:32:25 -0800

    Update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,70 +1,70 @@
 #!/usr/bin/python3
-"""Define the FileStorage class."""
+"""
+FileStorage class
+"""
+
 import json
-from models.base_model import BaseModel
 from models.amenity import Amenity
+from models.base_model import BaseModel
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
 
+classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
+
 
 class FileStorage:
-    """An abstracted storage engine.
+    """serializes instances to JSON file & deserializes to instances"""
 
-    Attributes:
-        __file_path (str): File to save objects.
-        __objects (dict): instantiated objects dictionary.
-    """
-
+    # string - JSON file path.
     __file_path = "file.json"
+    # dictionary - Store all objects by <class name>.id
     __objects = {}
 
     def all(self, cls=None):
-        """Dictionary of instantiated objects in __objects.
-
-        Return a dictionary of objects of that type, if a cls is specified.
-        else, return the __objects dictionary.
-        """
+        """return the dictionary __objects"""
         if cls is not None:
-            if type(cls) == str:
-                cls = eval(cls)
-            cls_dict = {}
-            for k, v in self.__objects.items():
-                if type(v) == cls:
-                    cls_dict[k] = v
-            return cls_dict
+            new_dict = {}
+            for key, value in self.__objects.items():
+                if cls == value.__class__ or cls == value.__class__.__name__:
+                    new_dict[key] = value
+            return new_dict
         return self.__objects
 
     def new(self, obj):
-        """ __objects obj with key <obj_class_name>.id."""
-        self.__objects["{}.{}".format(type(obj).__name__, obj.id)] = obj
+        """set __objects the obj with key <obj class name>.id"""
+        if obj is not None:
+            key = obj.__class__.__name__ + "." + obj.id
+            self.__objects[key] = obj
 
     def save(self):
-        """__objects to the JSON file __file_path."""
-        odict = {o: self.__objects[o].to_dict() for o in self.__objects.keys()}
-        with open(self.__file_path, "w", encoding="utf-8") as f:
-            json.dump(odict, f)
+        """serialize __objects to JSON (path: __file_path)"""
+        json_objects = {}
+        for key in self.__objects:
+            json_objects[key] = self.__objects[key].to_dict()
+        with open(self.__file_path, 'w') as f:
+            json.dump(json_objects, f)
 
     def reload(self):
-        """Deserialize the JSON file __file_path to __objects."""
+        """deserialize JSON to __objects"""
         try:
-            with open(self.__file_path, "r", encoding="utf-8") as f:
-                for o in json.load(f).values():
-                    name = o["__class__"]
-                    del o["__class__"]
-                    self.new(eval(name)(**o))
-        except FileNotFoundError:
+            with open(self.__file_path, 'r') as f:
+                jo = json.load(f)
+            for key in jo:
+                self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
+        except:
             pass
 
     def delete(self, obj=None):
-        """Delete given object from __objects."""
-        try:
-            del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
-        except (AttributeError, KeyError):
-            pass
+        """ewmove obj from __objects if it’s inside"""
+        if obj is not None:
+            key = obj.__class__.__name__ + '.' + obj.id
+            if key in self.__objects:
+                del self.__objects[key]
 
     def close(self):
-        """Call the reload method."""
+        """call reload() to deserialize the JSON file to objects"""
         self.reload()
